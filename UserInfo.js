1e4e5c1fada121cd29954b183bd6a796f3e8e064
0x727AA7D <120040061@link.cuhk.edu.cn>
2022-11-18 02:23:50 +0800

    Update UserInfo

--- UserInfo.js
+++ UserInfo.js
@@ -1,6 +1,11 @@
-// 离校相关
-var my_d_leaveDate = "2022-11-17" // 离校时间，格式如 2022-06-29
-var my_d_backDate = "2022-11-18" // 返校时间，格式如 2022-06-29
+// 个人信息
+var my_stuID = "120040061"; // 学生ID
+var my_stuName = "汪宁远"; // 姓名
+var my_dormNo = "A304"; // 宿舍号
+var my_stuCollegeName = "数据科学学院"; // 学院全称
+var my_stuUnitName = "思廷书院" // 书院全称
+var my_phone = "13712039821" // 联系电话
+var my_stuUnitName_eng = "MUSE" // 书院,格式如 MUSE
 
 // !!! 注意: 有效天数检查已关闭，请确保 返校时间 在 申请有效期 之内 !!!
 var my_valid_day_cnt = "7" // 申请有效期，默认为7，(别改太离谱的QwQ)
@@ -12,11 +17,17 @@ var my_street = "启迪协信科技园" // 所在街道以及详细地址
 
 var my_remark = "洽谈社团事务" // 离校备注/原因
 
-// 个人信息
-var my_stuID = "1200400XX"; // 学生ID
-var my_stuName = "XXX"; // 姓名
-var my_dormNo = "AXXX"; // 宿舍号
-var my_stuCollegeName = "数据科学学院"; // 学院全称
-var my_stuUnitName = "思廷书院" // 书院全称
-var my_phone = "137XXXXYYYY" // 联系电话
-var my_stuUnitName_eng = "MUSE" // 书院,格式如 MUSE
+
+// TODAY
+var day = new Date();
+day.setTime(day.getTime());
+var s = day.getFullYear()+"-" + (day.getMonth()+1) + "-" + day.getDate();
+
+// NEXT DAY
+var day2 = new Date();
+day2.setTime(day2.getTime()+24*60*60*1000);
+var s2 = day2.getFullYear()+"-" + (day2.getMonth()+1) + "-" + day2.getDate();
+
+// 离校相关时间
+var my_d_leaveDate = s // 离校时间，格式如 2022-06-29
+var my_d_backDate = s2 // 返校时间，格式如 2022-06-29
